e014cdf5039b65288be629aea5180423d7035341
Alessandro Angioi <alessandro.angioi@hotmail.com>
2020-09-03 22:05:48 +0200

    Add mock producer

---
+++ producer/test_producer.py
@@ -0,0 +1,16 @@
+import unittest
+from .producer import app
+
+
+class TestProducer(unittest.TestCase):
+
+    def test_index(self):
+        response = app.test_client().get('/')
+        self.assertEqual(response.status_code, 200)
+
+        body = response.json['body']
+        self.assertEqual(body, "Thanks for visiting!")
+
+
+if __name__ == '__main__':
+    unittest.main()
