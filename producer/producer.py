b4b57ddbd4f67cd100220c483b82914c9fbecdb6
Alessandro Angioi <alessandro.angioi@hotmail.com>
2020-09-04 07:59:40 +0200

    Add pika code to producer; network does not seem to work

--- producer/producer.py
+++ producer/producer.py
@@ -1,7 +1,20 @@
 from flask import Flask
+import pika
+import atexit
+
 app = Flask(__name__)
 
+# Setup of connection
+connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+# Connection must be closed when we are done
+atexit.register(connection.close)
 
 @app.route('/')
 def send_message():
+    channel.basic_publish(routing_key='hello',
+                          body='Hey bud, someone visited me',
+                          exchange='')
     return {'body': 'Thanks for visiting!'}, 200
